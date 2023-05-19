2b342c800b383a1589e71930036a3e2520fa87e4
MD Nahasat Hossain Nibir <nahasat.nibir@gmail.com>
2023-05-19 13:37:24 +0600

    Update index.js

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ app.use(express.static("public"));
 //ROUTES//
 
 // ----------------------------------------------
-app.listen(7138, () => {
-  console.log("server has started on port 5000");
+app.listen(3000,"0.0.0.0", () => {
+  console.log("server has started on port 3000");
 });
 
 app.use("/", (req, res) => {
